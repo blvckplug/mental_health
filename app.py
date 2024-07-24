a4d18b420aa8e5397d7f05350a78b14a24b95dcf
blvckplug <raggaayo30@gmail.com>
2024-07-24 01:56:10 +0100

    app.py

--- app.py
+++ app.py
@@ -1,31 +1,38 @@
-from flask import Flask,request, url_for, redirect, render_template
+from flask import Flask, request, render_template
 import pickle
 import numpy as np
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__, template_folder='templates')
 
-model=pickle.load(open('model.pkl','rb'))
+# Load the model
+model = pickle.load(open('model.pkl', 'rb'))
 
-
+# Define routes
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template('index.html')
 
-
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=model.predict_proba(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
+    # Get the values from the form
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
 
-    if output>str(0.5):
-        return render_template('index.html',pred='You need a treatment.\nProbability of mental illness is {}'.format(output))
+    # Make prediction
+    prediction = model.predict_proba(final_features)
+    probability = prediction[0][1]  # Probability of needing treatment
+
+    # Format the probability to two decimal places
+    formatted_prob = '{:.2f}'.format(probability)
+
+    # Determine the prediction message
+    if probability > 0.5:
+        result = 'You need treatment. Probability of mental illness is {}'.format(formatted_prob)
     else:
-        return render_template('index.html',pred='You do not need treatment.\n Probability of mental illness is {}'.format(output))
+        result = 'You do not need treatment. Probability of mental illness is {}'.format(formatted_prob)
 
+    # Render the result to the index.html template
+    return render_template('index.html', prediction_text=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
